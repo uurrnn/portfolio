ee266a98f44e6b86d19f8f3417f891eb9d51d689
uurrnn <uurrnn@gmail.com>
2015-06-11 23:31:48 -0400

    Adding TopShelf

---
+++ inventory/barback.mix.js
@@ -0,0 +1,7 @@
+// =============================================================================
+// ADD YOUR GLOBAL SCRIPTS TO THIS FILE TO COMBINE / REDUCE HTTP REQUESTS
+// =============================================================================
+
+//Combine all our scripts into one file
+@import "recipes/jquery/dist/jquery.js";
+@import "recipes/modernizr/modernizr.js";
