7ff0e003e7fb68b2e34d65bb9c925c3122c1a573
uurrnn <uurrnn@gmail.com>
2015-06-14 17:09:15 -0400

    Setting up github page for site

--- README.md
+++
@@ -1,3 +0,0 @@
-### My Portfolio
-
-This will be my portfolio, hosted on uurrnn.com
--- inventory/barback.mix.js
+++
@@ -1,7 +0,0 @@
-// =============================================================================
-// ADD YOUR GLOBAL SCRIPTS TO THIS FILE TO COMBINE / REDUCE HTTP REQUESTS
-// =============================================================================
-
-//Combine all our scripts into one file
-@import "recipes/jquery/dist/jquery.js";
-@import "recipes/modernizr/modernizr.js";
